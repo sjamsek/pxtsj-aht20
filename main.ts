db88ed72d59d3443dfb601e977d0e8128cb80323
sjamsek <117854233+sjamsek@users.noreply.github.com>
2025-11-28 15:24:30 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,101 @@
 //% weight=100 color=#32a852 icon="\uf2c9" block="AHT20"
 namespace AHT20Sensor {
 
-    const ADDR = 0x38;
-    const CMD_INIT = [0xBE, 0x08, 0x00];
-    const CMD_MEASURE = [0xAC, 0x33, 0x00];
-    const CMD_RESET = 0xBA;
-    const CRC_INIT = 0xFF;
-    const CRC_POLY = 0x31;
+    import { sleep, i2c, display, temperature } from "microbit";
 
-    export class AHT20 {
-        private addr: number;
-        private isChecksum: boolean;
-        private isCalibrated: boolean;
+const ADDR = 0x38;
+const CMD_INIT = [0xBE, 0x08, 0x00];
+const CMD_MEASURE = [0xAC, 0x33, 0x00];
+const CMD_RESET = 0xBA;
+const CRC_INIT = 0xFF;
+const CRC_POLY = 0x31;
 
-        constructor(addr: number = ADDR) {
-            this.addr = addr;
-            this.isChecksum = false;
-            this.sleep(100);
-            this.initialise();
-            this.sleep(10);
-            this.isCalibrated = (this.readStatus() & 0b00001000) != 0;
-        }
+class AHT20 {
+    addr: number;
+    IsChecksum: boolean;
+    IsCalibrated: number;
 
-        private sleep(ms: number) {
-            control.waitMicros(ms * 1000);
-        }
-        private initialise(): void {
-            pins.i2cWriteBuffer(this.addr, pins.createBufferFromArray(CMD_INIT));
-        }
-        private readStatus(): number {
-            let buf = pins.i2cReadBuffer(this.addr, 1);
-            return buf[0];
-        }
+    constructor(addr = ADDR) {
+        sleep(100);
+        this.addr = addr;
+        this.IsChecksum = false;
+        this.Initialise();
+        sleep(10);
+        this.IsCalibrated = this.Read_Status() & 0b00001000;
+    }
 
-        //% blockId="aht20_is_calibrated" block="sensor is calibrated"
-        isCalibratedStatus(): boolean {
-            return this.isCalibrated;
+    Initialise() {
+        i2c.write(this.addr, Uint8Array.from(CMD_INIT));
+    }
+
+    Read_Status(): number {
+        const buf = i2c.read(this.addr, 1);
+        return buf[0];
+    }
+
+    Is_Calibrated(): boolean {
+        return Boolean(this.IsCalibrated);
+    }
+
+    Is_Checksum(): boolean {
+        return this.IsChecksum;
+    }
+
+    Read(): [number, number, boolean] {
+        i2c.write(this.addr, Uint8Array.from(CMD_MEASURE));
+        sleep(80);
+        let busy = true;
+        while (busy) {
+            sleep(10);
+            busy = (this.Read_Status() & 0b10000000) !== 0;
         }
+        const buf = i2c.read(this.addr, 7);
+        const measurements = this._Convert(buf);
+        return measurements;
+    }
+
+    T(): number {
+        const measurements = this.Read();
+        return Math.round(measurements[1] * 10) / 10;
+    }
+
+    RH(): number {
+        const measurements = this.Read();
+        return Math.floor(measurements[0] + 0.5);
+    }
+
+    Reset() {
+        i2c.write(this.addr, Uint8Array.of(CMD_RESET));
+        sleep(20);
+    }
+
+    private _Convert(buf: Uint8Array): [number, number, boolean] {
+        const RawRH = ((buf[1] << 16) | (buf[2] << 8) | buf[3]) >>> 4;
+        const RH = RawRH * 100 / 0x100000;
+        const RawT = ((buf[3] & 0x0F) << 16) | (buf[4] << 8) | buf[5];
+        const T = ((RawT * 200) / 0x100000) - 50;
+        this.IsChecksum = this._Compare_Checksum(buf);
+        return [RH, T, this.IsChecksum];
+    }
+
+    private _Compare_Checksum(buf: Uint8Array): boolean {
+        let check = CRC_INIT;
+        for (let i = 0; i < 6; i++) {
+            check ^= buf[i];
+            for (let x = 0; x < 8; x++) {
+                if ((check & 0x80) !== 0) {
+                    check = ((check << 1) ^ CRC_POLY) & 0xFF;
+                } else {
+                    check = (check << 1) & 0xFF;
+                }
+            }
+        }
+        return check === buf[6];
+    }
+}
+
+const aht20 = new AHT20();
+}
 
         //% blockId="aht20_read_temperature" block="read temperature (°C)"
         temperature(): number {
@@ -62,36 +121,5 @@ namespace AHT20Sensor {
             let measurements = this.convert(buf);
             return measurements;
         }
-
-        // Other internal methods omitted for brevity...
-
-        private convert(buf: Buffer): [number, number, boolean] {
-            let rawRH = ((buf[1] << 16) | (buf[2] << 8) | buf[3]) >>> 4;
-            let RH = rawRH * 100 / 0x100000;
-            let rawT = ((buf[3] & 0x0F) << 16) | (buf[4] << 8) | buf[5];
-            let T = ((rawT * 200) / 0x100000) - 50;
-            this.isChecksum = this.compareChecksum(buf);
-            return [RH, T, this.isChecksum];
-        }
-
-        private compareChecksum(buf: Buffer): boolean {
-            let check = CRC_INIT;
-            for (let i = 0; i < 6; i++) {
-                check ^= buf[i];
-                for (let bit = 0; bit < 8; bit++) {
-                    if (check & 0x80) {
-                        check = ((check << 1) ^ CRC_POLY) & 0xFF;
-                    } else {
-                        check = (check << 1) & 0xFF;
-                    }
-                }
-            }
-            return check == buf[6];
-        }
-    }
-
-    //% blockId="aht20_create" block="create AHT20 sensor"
-    export function createSensor(): AHT20 {
-        return new AHT20();
     }
 }
